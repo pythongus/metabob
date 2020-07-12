54c391e773d57d0ce4e1d3509ab9dd62af102856
Gustavo Garcia <pythongus@gmail.com>
2020-07-12 23:01:46 +1000

    Updated linked list

--- hackerrank/linked_list.py
+++ hackerrank/linked_list.py
@@ -1,29 +1,71 @@
 """
-Binary Tree Module
+A double linked list with head of the list.
+
+The value of the head of the list does not count as an element of the
+list.
 """
 from typing import Optional
 from hackerrank.node import Node
 
 
 class LinkedList:
+    """The implementation of a linked list."""
 
-    def __init__(self, head: Node = None):
-        self.head = head
-        self._current_node = head
+    def __init__(self):
+        self.head = Node(None)
+        self.head.left = self.head.right
+        self._current_node = self.head
+        self._len = 0
+        self._index = 0
 
     def is_empty(self) -> bool:
-        return self.head is None
+        return self.head.right is None
 
-    def next_element(self) -> Optional[Node]:
+    def push(self, element: Node):
+        """Appends an element to the end of the list."""
+        last_element = self.head.left
+        if last_element:
+            last_element.right = element
+            element.left = last_element
+        self.head.left = element
+        element.right = self.head
+        if self.is_empty():
+            self.head.right = element
+            element.left = self.head
+        self._len += 1
+
+    def next(self) -> Optional[Node]:
+        """A next element is always to the right of the current
+        element."""
         if self._current_node:
-            node = self._current_node
+            element = self._current_node
             self._current_node = self._current_node.right
-            return node
+            return self._current_node if self._current_node != self.head else self.head.right
         return None
 
-    def previous_element(self) -> Optional[Node]:
+    def rnext(self) -> Optional[Node]:
+        """Next element to the left of the current element of the list"""
         if self._current_node:
-            node = self._current_node
+            element = self._current_node
             self._current_node = self._current_node.left
-            return node
+            return self._current_node if self._current_node != self.head else self.head.left
         return None
+
+    def pop(self):
+        if self.head:
+            self._len -= 1
+            return self.head.left
+        return None
+
+    def __len__(self):
+        return self._len
+
+    def __iter__(self):
+        if self._current_node == self.head:
+            self._current_node = self.head.right
+        while self._current_node != self.head:
+            element = self._current_node
+            self._current_node = self._current_node.right
+            yield element
+
+        self._current_node = self.head.right
--- test/test_linked_list.py
+++ test/test_linked_list.py
@@ -2,15 +2,83 @@
 Linked List Unit Tests
 """
 from hackerrank.linked_list import LinkedList
+from hackerrank.node import Node
 
 
-def test_empty_list():
-    a_list = LinkedList()
-    assert a_list.is_empty()
+class TestLinkedList:
 
+    def test_empty_list(self):
+        a_list = LinkedList()
+        assert a_list.is_empty()
 
-def test_list_with_one_element():
-    a_list = LinkedList(1)
-    assert a_list.head.value == 1
-    assert not a_list.next_element()
-    assert not a_list.previous_element()
+    def test_list_with_one_element(self):
+        a_list = LinkedList()
+        elem_1 = Node(1)
+        a_list.push(elem_1)
+        assert a_list.next() == elem_1
+
+    def test_next_with_empty_list(self):
+        a_list = LinkedList()
+        assert not a_list.next()
+
+    def test_rnext_with_empty_list(self):
+        a_list = LinkedList()
+        assert not a_list.rnext()
+
+    def test_list_with_two_elements(self):
+        elem_1 = Node(1)
+        elem_2 = Node(2)
+        a_list = LinkedList()
+        a_list.push(elem_1)
+        a_list.push(elem_2)
+        assert a_list.next() == elem_1
+        assert a_list.next() == elem_2
+        assert a_list.rnext() == elem_1
+        assert a_list.rnext() == elem_2
+
+    def test_pop_with_no_elements(self):
+        a_list = LinkedList()
+        assert not a_list.pop()
+
+    def test_pop_with_one_element(self):
+        a_list = LinkedList()
+        elem_1 = Node(1)
+        a_list.push(elem_1)
+        element = a_list.pop()
+        assert elem_1 == element
+
+    def test_pop_with_two_elements(self):
+        a_list = LinkedList()
+        elem_1 = Node(1)
+        elem_2 = Node(2)
+        a_list.push(elem_1)
+        a_list.push(elem_2)
+        assert len(a_list) == 2
+        a_list.pop()
+        assert len(a_list) == 1
+        element = a_list.pop()
+        assert len(a_list) == 0
+        assert elem_2 == element
+
+    def test_list_len_with_empty_list(self):
+        a_list = LinkedList()
+        assert len(a_list) == 0
+
+    def test_list_len(self):
+        a_list = LinkedList()
+        for i in range(5):
+            a_list.push(Node(i))
+        assert len(a_list) == 5
+
+    def test_list_iter(self):
+        a_list = LinkedList()
+        for i in range(5):
+            a_list.push(Node(i))
+        for index, e in enumerate(a_list):
+            assert isinstance(e, Node)
+        assert index == 4
+        counter = 0
+        for e in a_list:
+            counter += 1
+            assert isinstance(e, Node)
+        assert counter == 5
