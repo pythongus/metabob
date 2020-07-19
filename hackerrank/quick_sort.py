c50720626aed6cc4de74aaa23254d0a9c1c7a0e5
Gustavo Garcia <pythongus@gmail.com>
2020-07-20 07:13:34 +1000

    Refactored quick_sort to use hypothesis

--- hackerrank/quick_sort.py
+++ hackerrank/quick_sort.py
@@ -4,11 +4,15 @@ Classic Quick Sort Algorithm
 
 
 def quick_sort(elements: list):
+    """Sorts the elements in the given list using the
+    quick sort algorithm.
+    """
     qsort(elements, 0, len(elements) - 1)
     return elements
 
 
 def qsort(elems:list, lo: int, hi: int):
+    """Executes the recursive call to the partitions"""
     if lo < hi:
         pi = partition(elems, lo, hi)
         qsort(elems, lo, pi - 1)
@@ -16,6 +20,9 @@ def qsort(elems:list, lo: int, hi: int):
 
 
 def partition(elems, lo, hi) -> int:
+    """Sorts the elements in the partition given by lo(w) and
+    hi(gh) ends.
+    """
     i = lo - 1
     pivot = elems[hi]
     for j in range(lo, hi):
@@ -28,4 +35,5 @@ def partition(elems, lo, hi) -> int:
 
 
 def swap(elems: list, first: int, second: int):
+    """Swaps the position of two elements"""
     elems[first], elems[second] = elems[second], elems[first]
